dbfa49b498d6c9efd4722c01e30d769e9a5d78b8
Huntcool1 <huntercoull88@gmail.com>
2026-04-10 07:11:42 +0200

    Create rules.py

---
+++ src/rules.py
@@ -0,0 +1,9 @@
+# Simple improvement rules (expandable system)
+
+IMPROVEMENTS = [
+    "Use meaningful variable names",
+    "Avoid unnecessary loops when list comprehensions are possible",
+    "Reduce repeated calculations",
+    "Prefer built-in functions for performance",
+    "Break large functions into smaller ones"
+]
